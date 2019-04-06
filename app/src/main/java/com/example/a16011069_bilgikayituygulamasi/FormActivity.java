d1477ce087ab9832760367cc291b25ebbbabf39a
bilal <bilalguvenc98@gmail.com>
2019-04-07 00:46:15 +0300

    2 activities finished (main and form)

--- app/src/main/java/com/example/a16011069_bilgikayituygulamasi/FormActivity.java
+++ app/src/main/java/com/example/a16011069_bilgikayituygulamasi/FormActivity.java
@@ -1,13 +1,57 @@
 package com.example.a16011069_bilgikayituygulamasi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class FormActivity extends AppCompatActivity {
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class FormActivity extends AppCompatActivity
+{
+    EditText eName, eSurname, eId, eMail, eTel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        eName = findViewById(R.id.etName);
+        eSurname = findViewById(R.id.etSurname);
+        eId = findViewById(R.id.etId);
+        eMail = findViewById(R.id.etMail);
+        eTel = findViewById(R.id.etTel);
+    }
+
+    public void clearAll (View view)
+    {
+        eName.setText("");
+        eSurname.setText("");
+        eId.setText("");
+        eMail.setText("");
+        eTel.setText("");
+    }
+
+    public void save (View v)
+    {
+        String name, surname, id, mail, tel;
+
+        name = eName.getText().toString();
+        surname = eSurname.getText().toString();
+        id = eId.getText().toString();
+        mail = eMail.getText().toString();
+        tel = eTel.getText().toString();
+
+
+        Intent intent = new Intent(this, ListActivity.class);
+
+        intent.putExtra ("name", name);
+        intent.putExtra ("surname", surname);
+        intent.putExtra ("id", id);
+        intent.putExtra ("mail", mail);
+        intent.putExtra ("tel", tel);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/a16011069_bilgikayituygulamasi/ListActivity.java
+++ app/src/main/java/com/example/a16011069_bilgikayituygulamasi/ListActivity.java
@@ -1,13 +1,44 @@
 package com.example.a16011069_bilgikayituygulamasi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity
+{
+    TextView eName, eSurname, eId, eMail, eTel;
+    String name, surname, id, mail, tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        init();
+    }
+
+    void init ()
+    {
+        Intent i = getIntent ();
+
+        name = (String) i.getSerializableExtra ("name");
+        surname = (String) i.getSerializableExtra ("surname");
+        id = (String) i.getSerializableExtra ("id");
+        mail = (String) i.getSerializableExtra ("mail");
+        tel = (String) i.getSerializableExtra ("tel");
+
+        eName = findViewById (R.id.tvName);
+        eSurname = findViewById (R.id.tvSurname);
+        eId = findViewById (R.id.tvId);
+        eMail = findViewById (R.id.tvMail);
+        eTel = findViewById (R.id.tvPhone);
+
+        eName.setText(name);
+        eSurname.setText(surname);
+        eId.setText(id);
+        eMail.setText(mail);
+        eTel.setText(tel);
     }
 }
